ec0ba6cb12e7981a59370f4c4e9c74f53448c6c5
Felipe Correia Costa <ra51415081883@cursos.insf>
2024-07-30 15:28:58 -0300

    iniciando o projeto

---
+++ src/pages/inicio/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+
+
+export default function Inicio() {
+  return (
+    <div className="App">
+      <header className="pagina-inicio">
+      <img src='/assets/images/frei-logo.png' alt=''/>
+      <h2>React FreiS</h2>
+      
+      </header>
+    </div>
+  );
+}
---
+++ src/pages/sobre/Sobre.jsx
@@ -0,0 +1,9 @@
+import './Sobre.scss'
+
+export default function Sobre(){
+    return(
+        <div className='pagina-sobre'>
+            <p>as</p>
+        </div>
+    )
+}
